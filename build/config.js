6973b6da116ba6b2e745074c166c0200b4e49c6e
葛欣 <gexin@hw-mail.cn>
2020-04-09 14:49:27 +0800

    优化项目

--- build/config.js
+++ build/config.js
@@ -2,7 +2,7 @@
  * @Author: river
  * @Date: 2020-04-09 11:33:23
  * @Last Modified by: river
- * @Last Modified time: 2020-04-09 14:09:04
+ * @Last Modified time: 2020-04-09 14:46:20
  */
 const { resolve } = require('./webpack.help');
 module.exports = {
@@ -30,7 +30,7 @@ module.exports = {
     devServer: {
       contentBase: './dist',
       stats: 'errors-only',
-      compress: true,
+      compress: false,
       host: 'localhost',
       port: 7001,
       open: true
